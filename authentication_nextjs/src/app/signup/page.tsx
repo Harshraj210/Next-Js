3389fe36f739d796dd245b377b9a5c98ffb09697
harsh <nh1750501@gmail.com>
2025-09-03 13:02:29 +0530

    updated code base

--- authentication_nextjs/src/app/signup/page.tsx
+++ authentication_nextjs/src/app/signup/page.tsx
@@ -1,5 +1,12 @@
-export default function SignUpPage(){
-  return (
-    <div></div>
-  )
+"use client";
+// adding this to top makes this a client component --> runs on browser only
+import Link from "next/link";
+import React from "react";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import axios from "axios";
+// better than fetch --> automatic JSON conversion
+
+export default function SignUpPage() {
+  return <div></div>;
 }
--- authentication_nextjs/src/dbconfig/dbconfig.ts
+++ authentication_nextjs/src/dbconfig/dbconfig.ts
@@ -4,6 +4,15 @@ export async function connect() {
   try {
     mongoose.connect(process.env.MONGO_URI!);
     const connection = mongoose.connection;
+    connection.on("connected", () => {
+      console.log("MongoDB connected successfully");
+    });
+    connection.on("error", (err) => {
+      console.log(
+        "MongoDB connection Error make sure MONGODB is running" + err
+      );
+      process.exit();
+    });
   } catch (error) {
     console.log("Soemthing went wrong!!");
     console.log(error);
