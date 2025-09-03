345a8d801cfc37107409d33d6966b10e7b879426
harsh <nh1750501@gmail.com>
2025-09-03 20:53:57 +0530

    Updated code base

--- authentication_nextjs/src/app/api/users/signup/route.ts
+++ authentication_nextjs/src/app/api/users/signup/route.ts
@@ -1,4 +1,5 @@
-import {connect} from "@/dbconfig/dbconfig"
+import { connect } from "@/dbconfig/dbconfig";
+import User from "@/models/usermodel.js";
+import { NextRequest, NextResponse } from "next/server";
 
-
-connect()
+connect();
