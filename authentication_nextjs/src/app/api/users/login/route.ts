a863f17c985e374812b99d3661f7ef4a0ee8fb59
harsh <nh1750501@gmail.com>
2025-09-04 20:06:59 +0530

    Creating tokens

--- authentication_nextjs/src/app/api/users/login/route.ts
+++ authentication_nextjs/src/app/api/users/login/route.ts
@@ -2,6 +2,7 @@ import { connect } from "@/dbconfig/dbconfig";
 import User from "@/models/usermodel";
 import bcrypt from "bcryptjs";
 import { NextRequest, NextResponse } from "next/server";
+import jwt from "jsonwebtoken";
 connect();
 
 export async function POST(request: NextRequest) {
@@ -18,7 +19,28 @@ export async function POST(request: NextRequest) {
       );
     }
     // pass is correct
-    
+    const validPassword = await bcrypt.compare(password, user.password);
+    if (!validPassword) {
+      return NextResponse.json({ error: "Invalid Password" }, { status: 500 });
+    }
+    // creating tokens data
+
+    const TokenData = {
+      id: user._id,
+      username: user.username,
+      email: user.email,
+    };
+    // creating tokens
+
+    const token = jwt.sign(TokenData, process.env.TOKEN_SECRET!, {
+      expiresIn: "1d",
+    });
+    const response = NextResponse.json({
+      message: "Login successful",
+      token,
+    });
+    response.cookies.set("token", token, { httpOnly: true });
+    return response;
   } catch (error: any) {
     return NextResponse.json({ error: error.message }, { status: 500 });
   }
