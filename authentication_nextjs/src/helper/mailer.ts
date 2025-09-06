5a381ee05cb8364c14f68b9ffa7c953492254d6d
harsh <nh1750501@gmail.com>
2025-09-06 19:13:19 +0530

    Updated code base

--- authentication_nextjs/src/helper/mailer.ts
+++ authentication_nextjs/src/helper/mailer.ts
@@ -6,11 +6,33 @@ export const sendmail = async ({ email, emailType, userId }: any) => {
   try {
     // creasting hashed token
     const Hashedtoken = await bcrypt.hash(userId.toString(), 10);
-    await User.findByIdAndUpdate(userId, {
-      VerifyToken: Hashedtoken,
-      verifyTokenExpiry: Date.now() + 3600000,
-    });
+    if (emailType === "VERIFY") {
+      await User.findByIdAndUpdate(userId, {
+        VerifyToken: Hashedtoken,
+        verifyTokenExpiry: Date.now() + 3600000,
+      });
+    } else if (emailType === "RESET  ") {
+      await User.findByIdAndUpdate(userId, {
+        forgotPasswordToken: Hashedtoken,
+        forgotPasswordTokenExpiry: Date.now() + 3600000,
+      });
+    }
+    const transport =  nodemailer.createTransport({
+       'host' : 'sandbox.smtp.mailtrap.io',
+        'port' : 2525,
+        'username' : 'cdca3156754fe2',
+        'password' :'****8432',
+        'className' : 'Smtp'
+    })
+    const MailOptions={
+      from:"nh1750501@gmail.com",
+      to:email,
+      subject:emailType==="VERIFY"? "Verify your Email":"Reset your Password",
+      html:`<p>Click <a href =${process.env.domain}/verifyemail?token=${Hashedtoken}">Here </a> to ${emailType==="VERIFY"? "Verify your Email":"Reset your Password"}</p>`
+    }
+    const mailresponse = await transport.sendMail(MailOptions);
+        return mailresponse;
   } catch (error: any) {
-    throw new error(error.message);
+    throw new Error(error.message);
   }
 };
