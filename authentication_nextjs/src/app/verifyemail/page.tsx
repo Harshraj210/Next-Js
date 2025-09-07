412f89c5f18056b00dc08e0d4610b36d7a8877cc
harsh <nh1750501@gmail.com>
2025-09-07 13:47:28 +0530

    updarted code base

--- authentication_nextjs/src/app/verifyemail/page.tsx
+++ authentication_nextjs/src/app/verifyemail/page.tsx
@@ -1,25 +1,31 @@
-"sue client"
-import axios from "axios"
-import Link from "next/link"
-import React,{useEffect,useState} from "react"
+"sue client";
+import axios from "axios";
+import Link from "next/link";
+import React, { useEffect, useState } from "react";
 
-export default function verifyEmailPage(){
-  const [token,setToken]=useState("")
-  const [verified,setVerified]=useState(false)
-  const [error,setError]=useState(false)
+export default function verifyEmailPage() {
+  const [token, setToken] = useState("");
+  const [verified, setVerified] = useState(false);
+  const [error, setError] = useState(false);
 
-  const verifyuseremail=async()=>{
+  const verifyuseremail = async () => {
     try {
-      await axios.post("/api/user/verifyemail",{token})
-      setVerified(true)
-    } catch (error:any) {
-      setError(true),
-      console.log(error.response.data)
+      await axios.post("/api/user/verifyemail", { token });
+      setVerified(true);
+    } catch (error: any) {
+      setError(true), console.log(error.response.data);
     }
-  }
-  useEffect(()=>{
-    if(token.length>0){
-      verifyuseremail()
+  };
+
+  useEffect(() => {
+    const urltoken = window.location.search.split("=")[1];
+    setToken(urltoken)
+  }, []);
+  useEffect(() => {
+    if (token.length > 0) {
+      verifyuseremail();
     }
-  },[])
+  }, [token]);
+  return 
+  <div
 }
